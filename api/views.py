10439354bb4b802f55b13ee2dc0e123e4f978a15
jagrit-Indegenous <jagrit@theindegenous.org>
2023-01-18 20:43:58 +0530

    minor change

--- api/views.py
+++ api/views.py
@@ -55,6 +55,8 @@ def taskCreate(request):
 	serializer = TaskSerializer(data=request.data)
 	if serializer.is_valid():
 		serializer.save()
+	return Response(serializer.data)
+
 @api_view(['POST'])
 def Nifty_Update(request):
 	dtobj1 = datetime.datetime.utcnow()  # utcnow class method
