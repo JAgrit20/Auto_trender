c5b69567acd5750ff05f89f4972c537b83baf173
jagrit-Indegenous <65972513+JAgrit20@users.noreply.github.com>
2023-02-08 23:51:40 +0530

    new api added

--- counter/urls.py
+++ counter/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
         path('s3', views.strategy_3, name='strategy_3'),
         path('check', views.Check, name='check'),
         path('check_5min', views.check_5min, name='check_5min'),
+        path('check_s3', views.check_s3, name='check_s3'),
         path('check_both', views.Check_both, name='check'),
         # path('check_adx', views.check_adx, name='check_adx'),
         path('api/', include('api.urls')),
