52debc8aa1b0d7955cdf03ebdc937386096f342a
Jagrit Acharya <jagritacharya2020@gmail.com>
2023-03-03 05:17:32 +0530

    mc

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -164,7 +164,7 @@ def Telegram_data():
 	dtobj_india = dtobj_india.strftime("%H:%M")
 	dtobj_indiaa = str(dtobj_india)
 	print("count", count)
-	if(count >= 4):
+	if(count >= 40):
 		prev_spot = spot
 		spot = float(nifty_val)
 		b = float(spot/100)
